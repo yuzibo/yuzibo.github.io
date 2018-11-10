224da152ac8242df464d95593b17f29282107cca
Bo YU <tsu.yubo@gmail.com>
2018-11-10 09:27:15 +0800

    add upload.sh
    Signed-off-by: Bo YU <tsu.yubo@gmail.com>

---
+++ upload.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: upload.sh
+# Author: Bo Yu
+# mail: tsu.yubo@gmail.com
+# Created Time: Monday, May 16, 2016 AM10:02:04 HKT
+##########################################################################
+#!/bin/bash
+
+git add . && git commit -a && git push origin master
+
+
